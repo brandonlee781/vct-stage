da957e0130ec851cea41063ea5bbf475d0df13f2
Brandon Lee <brandonlee781@gmail.com>
2024-04-30 08:04:55 -0500

    Home page and base path

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,13 @@
 import type { FunctionComponent } from "@/lib/types";
+import { Link } from '@tanstack/react-router';
 
 export const Home = (): FunctionComponent => {
 	return (
-		<div className="bg-blue-300 font-bold w-screen h-screen flex flex-col justify-center items-center ">
-			<p className="text-white text-6xl">Hello, world!</p>
+		<div className="w-screen h-screen flex flex-col justify-center items-center text-lg font-bold">
+			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'pacific' }}>Pacific</Link>
+			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'emea' }}>EMEA</Link>
+			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'china' }}>China</Link>
+			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'americas' }}>Americas</Link>
 		</div>
 	);
 };
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vct-stage',
   plugins: [
     react(),
     TanStackRouterVite(),
