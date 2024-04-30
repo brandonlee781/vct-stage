0008823f8590ab2b19594b22806483adeafca505
Brandon Lee <brandonlee781@gmail.com>
2024-04-30 08:25:43 -0500

    Reorder home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,10 +4,10 @@ import { Link } from '@tanstack/react-router';
 export const Home = (): FunctionComponent => {
 	return (
 		<div className="w-screen h-screen flex flex-col justify-center items-center text-lg font-bold">
-			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'pacific' }}>Pacific</Link>
-			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'emea' }}>EMEA</Link>
-			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'china' }}>China</Link>
 			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'americas' }}>Americas</Link>
+			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'china' }}>China</Link>
+			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'emea' }}>EMEA</Link>
+			<Link className="mx-2" to="/league/$leagueId" params={{ leagueId: 'pacific' }}>Pacific</Link>
 		</div>
 	);
 };
